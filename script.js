73798f57fdeec5e5548e8d89f6eb3fdd9f21edf8
Umesh Deogharia <143274287+Umesh-Deogharia@users.noreply.github.com>
2024-10-20 21:47:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
 //your code here!
+let OrderList = document.getElementById('infi-list');
+let id = 1;
+function load() {
+	for (let i = 0; i < 20; i++) {
+	let list = document.createElement('li');
+	list.innerHTML = `Item ${id++}`;
+		OrderList.appendChild(list);
+}
+}
 
+OrderList.addEventListener('scroll', function() {
+if (OrderList.scrollTop + OrderList.clientHeight >= OrderList.scrollHeight) {
+	load();
+}	
+})
+load();
